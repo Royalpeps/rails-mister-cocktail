fe74b41bf7197021eca20679f7f7e993b6080a95
Perouze Thomas <thomasperouze@gmail.com>
2017-07-14 12:48:19 +0200

    "Fin de l'exercice"

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = @cocktail.doses.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = @cocktail.doses.find(params[:id])
+    @dose.destroy
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
+private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
